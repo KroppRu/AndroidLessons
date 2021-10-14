4c1653adfe8eba7a6dce7f98b8b7287e6c12594e
KroppRu <Kropp@Kropp.ru>
2021-10-14 22:32:52 +0700

    lesson13finish

--- lesson013simplemenu/src/main/java/ru/startandroid/lesson013simplemenu/MainActivity.java
+++ lesson013simplemenu/src/main/java/ru/startandroid/lesson013simplemenu/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.startandroid.lesson013simplemenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("alpha");
+        menu.add("beta");
+        menu.add("omega");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
